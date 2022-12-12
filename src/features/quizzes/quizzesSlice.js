1ba899a50d8fb37845bee8ff0e528523394a29f6
Mike <mike@mike-francis.org>
2022-12-12 09:05:49 +0000

    completed step 10 - hopefully

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const quizzesSlice = createSlice({
+  name: "quizzes",
+  initialState: {
+    quizzes: {}
+  },
+  reducers: {
+    addQuiz: (state, action) => {
+      const { id, name, topicId, cardIds } = action.payload;
+      state.quizzes[id] = {
+        id,
+        name,
+        topicId,
+        cardIds: []
+      };
+    }
+  }
+});
+
+export const { addQuiz } = quizzesSlice.actions;
+export const quizzesSelector = (state) => state.quizzes.quizzes;
+export default quizzesSlice.reducer;
